57752274a5b581df7429214947a615d929682237
GabrielcMiranda <gabriel24070036@aluno.cesupa.br>
2025-06-24 23:40:30 -0300

    feat: criacao da estrutura do projeto

---
+++ ps-backend/src/test/java/lapes/cesupa/ps_backend/PsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lapes.cesupa.ps_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
