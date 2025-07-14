1293712db2985400acdeefe85b4138f2635f20b4
GabrielcMiranda <gabriel24070036@aluno.cesupa.br>
2025-07-13 23:21:27 -0300

    feat: adicao da rota de profile

--- ps-backend/src/main/java/lapes/cesupa/ps_backend/controller/UserController.java
+++ ps-backend/src/main/java/lapes/cesupa/ps_backend/controller/UserController.java
@@ -5,18 +5,29 @@ import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.transaction.Transactional;
 import lapes.cesupa.ps_backend.dto.CreateUser;
+import lapes.cesupa.ps_backend.dto.ProfileResponse;
 import lapes.cesupa.ps_backend.model.Role;
 import lapes.cesupa.ps_backend.model.User;
 import lapes.cesupa.ps_backend.repository.RoleRepository;
 import lapes.cesupa.ps_backend.repository.UserRepository;
 
+import java.util.Collection;
 import java.util.Set;
+import java.util.UUID;
 
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Authentication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -53,5 +64,18 @@ public class UserController {
         
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("auth/profile")
+    public ResponseEntity<ProfileResponse> profile(@AuthenticationPrincipal Jwt jwt) {
+        var userId = jwt.getSubject();
+        var user = userRepository.findById(UUID.fromString(userId)).orElseThrow(() -> new BadCredentialsException("user not found"));
+
+        var roles = user.getRoles().stream().map(Role::getName).toList();
+
+        var profile = new ProfileResponse(user.getId(), user.getUsername(),user.getEmail(), roles);
+
+        return ResponseEntity.ok(profile);
+    }
+    
     
 }
