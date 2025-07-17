4924419a50687606ab44e611a74aa068f50b181b
GabrielcMiranda <gabriel24070036@aluno.cesupa.br>
2025-07-17 18:04:21 -0300

    fix: ajustando package da anotacao id nos models de item

--- ps-backend/src/main/java/lapes/cesupa/ps_backend/model/ItemImage.java
+++ ps-backend/src/main/java/lapes/cesupa/ps_backend/model/ItemImage.java
@@ -1,11 +1,10 @@
 package lapes.cesupa.ps_backend.model;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
